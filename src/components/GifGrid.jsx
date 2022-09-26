1d7b3e29a6740caffced5fed3398e0c4be67338f
practica <prueba@gmail.com>
2022-09-26 11:04:53 -0600

    qwe2

--- src/components/GifGrid.jsx
+++ src/components/GifGrid.jsx
@@ -1,12 +1,16 @@
 
 import { GifItem } from './GifItem'
 import { useFetchGifs } from '../hooks/useFetchGifs'
+import PropTypes from 'prop-types';
 
 // https://api.giphy.com/v1/gifs/search?api_key=uGrjbURJnoZDKb5xysNp2bUD8ayZqDw7&q=OnePiece&limit=16
 
 export const GifGrid = ({category}) =>{
 
     const {images, isLoading} = useFetchGifs(category)
+
+    console.log(category)
+    console.log(typeof category);
 
     return (
         <>
